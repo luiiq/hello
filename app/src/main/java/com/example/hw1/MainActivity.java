18f039f5fa920ccf6a365e552dd9f071c083457d
luiiq <luisquiroz7@icloud.com>
2019-02-03 23:40:25 -0500

    helloworld

--- app/src/main/java/com/example/hw1/MainActivity.java
+++ app/src/main/java/com/example/hw1/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.hw1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button myButton;
+    String myString = "This is my first toast:";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        myButton = (Button) findViewById(R.id.buttonToast);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), myString, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
